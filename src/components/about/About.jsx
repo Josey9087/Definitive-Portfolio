8d10d14714ab4389ee51b411e0ac13f8f99a8fdf
Josey9087 <88466226+Josey9087@users.noreply.github.com>
2022-03-12 17:27:02 -0700

    adding about render

---
+++ src/components/about/About.jsx
@@ -0,0 +1,31 @@
+import './about.css';
+
+const Intro = () => {
+    return (
+        <div className="intro">
+            <div className="intro-left">
+                <div className="introBox">
+                    <div className="introTitle">
+                        <div className="introName">Jose Yanez</div>
+                        <div className="intro-title">
+                            <div className="intro-title-wrapper">
+                                <div className="intro-title-item">Full Stack Developer 💻</div>
+                                <div className="intro-title-item">Team Player 💡</div>
+                                <div className="intro-title-item">Software Engineer 🧠</div>
+                                <div className="intro-title-item">Gamer 🎮</div>
+                            </div>
+                        </div>
+                        <div className="introAbout">
+                            A Full Stack Web Devloper currently pursuing his dreams of being a software developer.
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="intro-right">
+            </div>
+        </div>
+
+    )
+}
+
+export default Intro
