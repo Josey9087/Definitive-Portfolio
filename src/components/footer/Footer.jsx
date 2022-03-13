64a505080eb6af41251b6c00a6d49d3bf469b3df
Josey9087 <88466226+Josey9087@users.noreply.github.com>
2022-03-12 17:36:25 -0700

    adding footer render for icons:

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,18 @@
+import './footer.css'
+
+export const Footer = () => {
+  return (
+  <div className="foot">
+      <div className="footIcons">
+        <a href="https://github.com/Josey9087" target="_blank">
+        </a>
+        <a href="https://www.linkedin.com/in/jose-yanez-b5bb70223/" target="_blank">
+        </a>
+      </div>
+
+  </div>
+  )
+};
+
+
+export default Footer
