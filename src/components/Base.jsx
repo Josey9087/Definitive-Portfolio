65c5f24c575b6f4ec01877da719ed3fd8ac14d29
Josey9087 <88466226+Josey9087@users.noreply.github.com>
2022-03-12 17:26:26 -0700

    adding base container for all pages

---
+++ src/components/Base.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import Navbar from './navbar/Navbar';
+import About from './about/About';
+import Contact from './contact/Contact';
+import Portfolio from './portfolio/Portfolio';
+import Footer from './footer/Footer';
+import Resume from './resume/Resume'
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('About');
+
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+
+    return <About />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+      <Footer />
+    </div>
+  );
+}
