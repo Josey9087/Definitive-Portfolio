ab0298226bebd41ddac59a29957f8d28d82d59cc
Josey9087 <88466226+Josey9087@users.noreply.github.com>
2022-04-02 15:39:52 -0600

    updating image url

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export const projects = [
     {
         id: 1,
-        img: "https://user-images.githubusercontent.com/88466226/158243943-ab358455-c1dd-4b4a-809f-c8d4d3cbb641.png",
+        img: "https://user-images.githubusercontent.com/88466226/161401064-fa4e0ded-f409-4c24-8205-b179f695ec02.png",
         gitLink: "https://github.com/Josey9087/SoundBites", 
         siteLink: "https://josey9087.github.io/SoundBites/",
         title: "Sound Bites"
@@ -15,28 +15,28 @@ export const projects = [
     },
     {
         id: 3,
-        img:"https://user-images.githubusercontent.com/88465484/145938010-752d8392-384c-4c88-8cf4-ae38d35279bd.png",
+        img:"https://user-images.githubusercontent.com/88466226/161401125-43fe84b4-4990-4961-b14e-dcab6e54425b.png",
         gitink: "https://github.com/Josey9087/Dirt",
         siteLink: "", 
         title: "Dirt!"
     },
     {
         id: 4,
-        img: "https://user-images.githubusercontent.com/88466226/158244129-2c8a8ab7-6b06-4705-83cd-0ad6fa40cbde.png",
+        img: "https://user-images.githubusercontent.com/88466226/161401134-1f072271-c747-4f5e-b9d4-2fb3d584549a.png",
         gitLink: "https://github.com/Josey9087/NoteTaker",
         siteLink: "https://thawing-brushlands-23907.herokuapp.com/", 
         title: "Note Taker"
     },
     {
         id: 5,
-        img: "https://user-images.githubusercontent.com/88466226/158242418-06d302cb-6a65-4c9c-a936-5e0cd8703cd6.png",
+        img: "https://user-images.githubusercontent.com/88466226/161401050-6b42d406-ba9a-4f17-a65f-e2c02dcc2daf.png",
         gitLink: "https://github.com/Josey9087/TextEditor",
         siteLink: "https://infinite-woodland-39861.herokuapp.com/", 
         title: "Text Editor"
     },
     {
         id: 6,
-        img: "https://user-images.githubusercontent.com/88466226/158244219-8e829894-b472-4afe-a295-8377c5c81b2c.png",
+        img: "https://user-images.githubusercontent.com/88466226/161401138-06bb159c-4102-4321-9efc-9ff776cac747.png",
         gitLink: "https://github.com/Josey9087/TechBlog",
         siteLink: "https://protected-dusk-76372.herokuapp.com/", 
         title: "Tech Blog"
